c93ec0481f402e3e7fd7b2d55e232f52cfc0436a
gaeun <gaeun2937@gmail.com>
2022-03-28 21:15:57 +0900

    기능추가 1.할 일의 상태 변화, 2.즐겨찾기, 완료, 미완료 항목 모아보기

--- index/src/index.js
+++ index/src/index.js
@@ -4,6 +4,8 @@
 // const $btnAdd = $('.btnAdd');
 // const $listBox = $('#listBox');
 
+// getter, setter
+// https://ko.javascript.info/property-accessors#ref-2680
 
 
 const ToDoList = function (wrap) {
@@ -11,90 +13,198 @@ const ToDoList = function (wrap) {
     this.wrap = wrap;
     this.listBox = wrap.find('#listBox');
     this.btnAdd = wrap.find('.btnAdd');
+    this.btnMenu = wrap.find('#btnMenu');
+    this.menu = wrap.find('#menu');
+    this.listItem = [];
+    this.home = wrap.find('#home');
 
     class Item {
-        constructor(input){
+        constructor(input, id){
             this.inputVal = input;
-            this.check = 'false';
-            this.mark = 'false';
-            this.id = null;
-            this.html = this.template;
+            this._check = 'false';
+            this._mark = 'false';
+            this._id = id;
             this.wrap = null;
-        }
 
-        get template(){
-            const template = `
-            <div class="listItem bxShadow">
-                <div class="check pointer">
-                    <i class="fa-solid fa-check"></i>
-                </div>
-                <div class="content">
-                    <div class="todo NanumPS">${this.inputVal}</div>
-                    <div class="mark pointer">
-                        <i class="fa-regular fa-star"></i>
+            this._html = `
+                <div class="listItem bxShadow" id="${this._id}">
+                    <div class="check pointer">
+                        <i class="fa-solid fa-check"></i>
+                    </div>
+                    <div class="content">
+                        <div class="todo NanumPS">${this.inputVal}</div>
+                        <div class="mark pointer">
+                            <i class="fa-regular fa-star"></i>
+                        </div>
                     </div>
                 </div>
-            </div>
             `;
-
-            return template;
         }
 
-        initItem(wrap){
-            if(this.wrap !== null) return false;
-
-            this.wrap = wrap;
-            wrap.attr('id', this.id);
-
-
+        get id(){
+            return this._id;
         }
 
+        get html(){
+            return  this._html;
+        }
+
+        set html(value){
+            return this._html = value;
+        }
+
+        get check(){
+            return this._check;
+        }
+
+        set check(value){
+            this._check = value;
+        }
+
+        get mark(){
+            return this._mark;
+        }
+
+        set mark(value){
+            this._mark = value;
+        }
+
+        initItem(wrap, re){
+            const _self = this;
+            _self.wrap = wrap;
+
+            if(this.wrap === undefined){
+                console.log(`this.wrap: ${_self.wrap}}이 없어요.`);
+                return false;
+            };
+
+            if(!re) self.listItem.push(this);
+            
+            const $check = _self.wrap.find('.check');
+            const $mark = _self.wrap.find('.mark');
+
+            $check.on('click', function(){
+                $(this).toggleClass('on');
+                if($(this).hasClass('on')){
+                    _self.check = 'true';
+                }else{
+                    _self.check = 'false';
+                }
+            })
+
+            $mark.on('click', function(){
+                $(this).toggleClass('on');
+                const $icon = $(this).find('i');
+
+                if($(this).hasClass('on')){
+                    $icon.removeClass('fa-regular').addClass('fa-solid');
+                    _self.mark = 'true';
+
+                }else{
+                    $icon.removeClass('fa-solid').addClass('fa-regular');
+                    _self.mark = 'false';
+                }
+            })
+
+            this.wrap.find('.check, .mark').on('click', function(){
+                // console.log(`id: ${_self.id}, 완료: ${_self.check}, 즐찾: ${_self.mark}`);
+                _self.html = _self.wrap[0].outerHTML;
+            })
+        }
     };
 
-    this.listItem = [];
     this.count = 0;
+    this.data = {
+        get listBox(){
+            return this._listBox;
+        },
+
+        set listBox(value){
+            this._listBox = value;
+        }
+    }
 
     this.init = function () {
-        this.btnAdd.on('click', function(){
+        self.btnAdd.on('click', function(){
             const $userInput = $(this).parent('.userInput');
             const $inputText = $userInput.find('.inputText');
 
             if(self.isEmpty($inputText)) return false;
 
-            self.addListItem($inputText);
-            
+            self.updateItem($inputText);
         })
+
+        self.btnMenu.on('click', function(){
+            self.menu.toggleClass('on');
+        })
+
+        self.menu.find('.menuItem').on('click', function(){
+            const id = $(this).attr('id');
+            if(self.wrap.attr('view') === `page-${id}`) return false;
+            self.render(id);
+        })
+
+        self.home.on('click', function(){
+            const id = $(this).attr('id');
+            if(self.wrap.attr('view') === `page-${id}`) return false;
+            self.render(id);
+        });
+
     }
 
-    this.addListItem = function (el) {
-        const item = new Item(el.val());
-        self.listItem.push(item);
-        el.val('');
-        
-        self.appendHTML();
-    }
+    this.render = function(id){
+        self.menu.removeClass('on');
+        self.wrap.attr('view',`page-${id}`);
+        self.listBox[0].innerHTML = '';
 
-    this.appendHTML = function(){
-        const $obj = self.listItem[self.count];
-        const html = $obj.html;
-        self.listBox.append(html);
-        const $el = self.listBox.find('.listItem:last-child');
+        if(id === 'bookMark'){
+            self.loopItem('mark', 'true');
 
-        self.setObj($obj, $el);
-    }
+        }else if(id === 'comItem'){
+            self.loopItem('check', 'true');
 
-    this.setObj = function(obj, el){
-        const id = obj.id;
-
-        if(id === null){
-            obj.id = self.count+1;
-            obj.initItem(el);
-            self.count++;
-
-        }else{
-            return false;
+        }else if(id === 'inComitem'){
+            self.loopItem('check', 'false');
+            
+        }else if(id === 'home'){
+            self.loopItem();
         }
     }
+
+    this.loopItem= function(key, value){
+        const _key = key;
+        const _value = value;
+
+        if(key === undefined || value === undefined){
+            $.each(self.listItem, (index, item) =>{
+                self.listBox.append(item.html);
+                item.initItem(self.elItem(item.id), 're');
+            })
+            return false;
+        }
+
+        $.each(self.listItem, (index, item) =>{
+            if(item[_key] === value){
+                self.listBox.append(item.html);
+                item.initItem(self.elItem(item.id), 're');
+            }
+        })
+        
+    }
+
+    this.elItem = function(id){
+        return self.listBox.find(`#${id}`);
+    }
+
+    this.updateItem = function(el){
+        self.count++;
+        const item = new Item(el.val(), `list-${self.count}`);
+        el.val('');
+
+        self.listBox.append(item.html);
+        item.initItem(self.elItem(item._id));
+    }
+
 
     this.isEmpty = function(el){
         const str = el.val().trim();
